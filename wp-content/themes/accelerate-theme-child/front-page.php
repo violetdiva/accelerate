4f1ca1268448b0e8793b3fc21461f022a4a24565
violetdiva <violetdiva100@gmail.com>
2016-03-28 12:22:49 -0400

    more updates to front page & style sheet

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -28,19 +28,20 @@ get_header(); ?>
 <section class="featured-work">
 	<div class="site-content">
 		<h4>Featured Work</h4>
-			<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
- 			<?php while ( have_posts() ) : the_post(); 
- 				$image_1=get_field("image_1");
- 				$size="medium"; ?>
- 
+			<ul class="homepage-featured-work">
+				<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+ 				<?php while ( have_posts() ) : the_post(); 
+ 					$image_1=get_field("image_1");
+ 					$size="medium"; ?>
+ 			<li class="individual-featured-work">
  				<figure>
  					<?php echo wp_get_attachment_image( $image_1, $size ); ?>
  				</figure>
-
- 			<h3><a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a></h3>
-			<?php endwhile; // end of the loop. ?>
-			<?php wp_reset_query(); // resets the altered query back to the original ?>
-
+ 				<h3><a href="<?php the_permalink(); ?>" ><?php the_title(); ?></a></h3>
+ 			</li>
+				<?php endwhile; // end of the loop. ?>
+				<?php wp_reset_query(); // resets the altered query back to the original ?>
+			</ul>
 	</div>
 </section>
 
