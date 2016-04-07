69de4e1f5d2faf9108e2f796e67084a77f6362ef
violetdiva <violetdiva100@gmail.com>
2016-04-06 21:30:55 -0400

    added 404 & about

---
+++ wp-content/themes/accelerate-theme-child/page-about.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header(); ?>
+<div class="<?php if(is_page('contact-us')) {echo 'narrow-content'; } ?>">
+	<div id="primary" class="site-content ">
+		<div id="content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				
+			<div class='homepage-hero'>
+				<?php the_content(); ?>
+				
+			</div>
+		<?php endwhile; // end of the loop. ?>
+				<div class="title"><?php the_title(); ?></div>
+				<div class="content"><?php the_content(); ?></div>
+
+			<?php endwhile; // end of the loop. ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+</div>
+
+<?php get_footer(); ?>
